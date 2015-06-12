2586dcc182f5995ec639bf3bf1f760a02b14b555
Brent McSharry <brentm@adhb.govt.nz>
2015-06-13 01:55:53 +1200

    Further style editing

--- Hearts4Kids/Helpers/HelpBlockFor.cs
+++ Hearts4Kids/Helpers/HelpBlockFor.cs
@@ -6,12 +6,12 @@ namespace Hearts4Kids.Helpers
 {
     public static class MvcHtmlHelpers
     {
-        public static MvcHtmlString HelpBlockFor<TModel, TValue>(this HtmlHelper<TModel> self, Expression<Func<TModel, TValue>> expression)
+        public static MvcHtmlString DescriptionFor<TModel, TValue>(this HtmlHelper<TModel> self, Expression<Func<TModel, TValue>> expression)
         {
             var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
             var description = metadata.Description;
             if (string.IsNullOrEmpty(description)) { return MvcHtmlString.Empty; }
-            return MvcHtmlString.Create(string.Format(@"<p class='help-block col-md-10 col-md-push-2'>{0}</p>", description));
+            return MvcHtmlString.Create(description);
         }
     }
 }
