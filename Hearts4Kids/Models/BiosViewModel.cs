e47a6d1797618fd836bf58e2449e8d973a650c2c
Brent McSharry <brentm@adhb.govt.nz>
2015-06-12 12:05:25 +1200

    bio name attributes

--- Hearts4Kids/Models/BiosViewModel.cs
+++ Hearts4Kids/Models/BiosViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Hearts4Kids.Models
 {
@@ -6,12 +7,18 @@ namespace Hearts4Kids.Models
     {
         public int UserId { get; set; }
         public string Name { get; set; }
+
+        [Display(Name = "Citation Description", Description = "If we quote you, how would you like to be referred to")]
         [StringLength(128)]
         public string CitationDescription { get; set; }
+
         [DataType(DataType.MultilineText)]
         [StringLength(4000)]
+        [AllowHtml]
         public string Biography { get; set; }
+
         [StringLength(256)]
+        [Display(Name="Bio Picture",Description ="Picture to accompany your biography")]
         public string BioPicUrl { get; set; }
     }
 }
