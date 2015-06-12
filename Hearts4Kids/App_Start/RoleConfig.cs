2243f329a8fd223508f18e67e0795dd19419dc18
Brent McSharry <brentm@adhb.govt.nz>
2015-06-12 18:15:18 +1200

    missing files added

---
+++ Hearts4Kids/App_Start/RoleConfig.cs
@@ -0,0 +1,23 @@
+﻿using Hearts4Kids.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace Hearts4Kids
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole, int>
+    {
+        public ApplicationRoleManager(IRoleStore<ApplicationRole, int> roleStore)
+            : base(roleStore)
+        {
+        }
+
+        public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options, IOwinContext context)
+        {
+            var appRoleManager = new ApplicationRoleManager(new RoleStore<ApplicationRole,int, ApplicationUserRole>(context.Get<ApplicationDbContext>()));
+
+            return appRoleManager;
+        }
+    }
+}
---
+++ Hearts4Kids/Controllers/BiosController.cs
@@ -0,0 +1,104 @@
+﻿using Hearts4Kids.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Hearts4Kids.Models;
+
+namespace Hearts4Kids.Controllers
+{
+    [Authorize]
+    public class BiosController : Controller
+    {
+        // GET: Bios
+        public ActionResult CreateEditBio(int id=0)
+        {
+            if (id == 0) { id = User.Identity.GetUserId<int>(); }
+            else if (!IsAuthorised())
+            {
+                return RedirectToAction("Login", "Account", new { returnUrl = Request.RawUrl });
+            }
+            
+            return View(MemberDetailService.GetBioDetails(id));
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult CreateEditBio(BiosViewModel model)
+        {
+            if (!IsAuthorised())
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Unauthorized;
+                return RedirectToAction("Error");
+            }
+            if (ModelState.IsValid)
+            {
+                MemberDetailService.UpdateBios(model, User.Identity.GetUserId<int>(), ModelState);
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            return View(model);
+        }
+        public ActionResult UpdateDetails(int id=0)
+        {
+            var usr = GetUser(id);
+            if (string.IsNullOrEmpty(usr.PasswordHash))
+            {
+                RedirectToAction("Register","Account");
+            }
+            if (!IsAuthorised(usr.Id))
+            {
+                return RedirectToAction("Login", "Account", new { returnUrl = Request.RawUrl });
+            }
+            var returnView = MemberDetailService.GetMemberDetails(usr.Id) ?? new BioDetailsViewModel();
+            returnView.PhoneNumber = usr.PhoneNumber;
+            returnView.Email = usr.Email;
+            returnView.UserName = usr.UserName;
+            return View(returnView);
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult UpdateDetails(BioDetailsViewModel model)
+        {
+            if (!IsAuthorised())
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Unauthorized;
+                return RedirectToAction("Error");
+            }
+            if (ModelState.IsValid)
+            {
+                MemberDetailService.UpdateMemberDetails(model, User.Identity.GetUserId<int>(), ModelState);
+                //to do allow phone number update
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                
+            }
+            return View(model);
+        }
+        bool IsAuthorised(int id = 0)
+        {
+            return id == 0 || id == User.Identity.GetUserId<int>() || User.IsInRole(Domain.Admin);
+        }
+        ApplicationUser GetUser(int id=0)
+        {
+            using (var um = AccountController.GetApplicationUserManager())
+            {
+                ApplicationUser usr;
+                if (id == 0)
+                {
+                    usr = um.FindByName(User.Identity.Name);
+                }
+                else
+                {
+                    usr = um.FindById(id);
+                }
+                
+                return usr;
+            }
+        }
+    }
+}
