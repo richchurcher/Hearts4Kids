87f96a5fb6cce90e52a9b5c6195e2aa45cb25173
Rich Churcher <rich.churcher@gmail.com>
2017-02-25 12:35:24 -0800

    Testing and DI

---
+++ Hearts4Kids.Tests/Installers/ControllersInstallerTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Castle.Core;
+using Castle.Core.Internal;
+using Castle.MicroKernel;
+using Castle.Windsor;
+using Hearts4Kids.Controllers;
+using Hearts4Kids.Installers;
+using Xunit;
+
+namespace Hearts4Kids.Tests.Installers
+{
+    public class ControllersInstallerTests
+    {
+        private readonly IWindsorContainer _containerWithControllers;
+
+        private static IHandler[] GetAllHandlers(IWindsorContainer container)
+        {
+            return GetHandlersFor(typeof(object), container);
+        }
+
+        private static IHandler[] GetHandlersFor(Type type, IWindsorContainer container)
+        {
+            return container.Kernel.GetAssignableHandlers(type);
+        }
+
+        private static Type[] GetImplementationTypesFor(Type type, IWindsorContainer container)
+        {
+            return GetHandlersFor(type, container)
+                .Select(h => h.ComponentModel.Implementation)
+                .OrderBy(t => t.Name)
+                .ToArray();
+        }
+
+        private static Type[] GetPublicClassesFromApplicationAssembly(Predicate<Type> where)
+        {
+            return typeof(HomeController).Assembly.GetExportedTypes()
+                .Where(t => t.IsClass)
+                .Where(t => t.IsAbstract == false)
+                .Where(where.Invoke)
+                .OrderBy(t => t.Name)
+                .ToArray();
+        }
+
+        public ControllersInstallerTests()
+        {
+            _containerWithControllers = new WindsorContainer()
+                .Install(new ControllersInstaller());
+        }
+
+        [Fact]
+        public void All_controllers_implement_IController()
+        {
+            var allHandlers = GetAllHandlers(_containerWithControllers);
+            var controllerHandlers = GetHandlersFor(typeof(IController), _containerWithControllers);
+
+            Assert.NotEmpty(allHandlers);
+            Assert.Equal(allHandlers, controllerHandlers);
+        }
+
+        [Fact]
+        public void All_controllers_are_registered()
+        {
+            var allControllers = GetPublicClassesFromApplicationAssembly(c => c.Is<IController>());
+            var registeredControllers = GetImplementationTypesFor(typeof(IController), _containerWithControllers);
+            Assert.Equal(allControllers, registeredControllers);
+        }
+
+        [Fact]
+        public void All_and_only_controllers_have_Controllers_suffix()
+        {
+            var allControllers = GetPublicClassesFromApplicationAssembly(c => c.Name.EndsWith("Controller"));
+            var registeredControllers = GetImplementationTypesFor(typeof(IController), _containerWithControllers);
+            Assert.Equal(allControllers, registeredControllers);
+        }
+
+        [Fact]
+        public void All_and_only_controllers_live_in_Controllers_namespace()
+        {
+            var allControllers = GetPublicClassesFromApplicationAssembly(c => c.Namespace.Contains("Controllers"));
+            var registeredControllers = GetImplementationTypesFor(typeof(IController), _containerWithControllers);
+            Assert.Equal(allControllers, registeredControllers);
+        }
+
+        [Fact]
+        public void All_controllers_are_transient()
+        {
+            var nonTransientControllers = GetHandlersFor(typeof(IController), _containerWithControllers)
+                .Where(controller => controller.ComponentModel.LifestyleType != LifestyleType.Transient)
+                .ToArray();
+
+            Assert.Empty(nonTransientControllers);
+        }
+
+        [Fact]
+        public void All_controllers_expose_themselves_as_service()
+        {
+            var controllersWithWrongName = GetHandlersFor(typeof(IController), _containerWithControllers)
+                .Where(controller => controller.ComponentModel.Services.Single() != controller.ComponentModel.Implementation)
+                .ToArray();
+
+            Assert.Empty(controllersWithWrongName);
+        }
+    }
+}
--- Hearts4Kids/Global.asax.cs
+++ Hearts4Kids/Global.asax.cs
@@ -1,21 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using Hearts4Kids.Plumbing;
 
 namespace Hearts4Kids
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static IWindsorContainer _container;
+
+        private static void BootstrapContainer()
+        {
+            _container = new WindsorContainer()
+                .Install(FromAssembly.This());
+            var controllerFactory = new WindsorControllerFactory(_container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BootstrapContainer();
+        }
+
+        protected void Application_End()
+        {
+            _container.Dispose();
         }
     }
 }
