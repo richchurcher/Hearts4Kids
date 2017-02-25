22ef519d5a6f51f83ec9757b0ad2173e4c790a1b
Rich Churcher <rich.churcher@gmail.com>
2017-02-24 20:09:43 -0800

    Remove navigation properties
    * causing circular references in tests, probably not required

--- Hearts4Kids/Domain/MembersModel.Designer.cs
+++ Hearts4Kids/Domain/MembersModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\OEM\Documents\Visual Studio 2015\Projects\Hearts4Kids\Hearts4Kids\Domain\MembersModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\richc\.babun\cygwin\home\richc\w\Hearts4Kids\Hearts4Kids\Domain\MembersModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
