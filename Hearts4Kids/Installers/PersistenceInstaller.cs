9745c6c5ab4571d473f41be27430f16bac8d7a30
Rich Churcher <rich.churcher@gmail.com>
2017-02-25 22:17:54 -0800

    DI for DbContext

---
+++ Hearts4Kids/Installers/PersistenceInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Hearts4Kids.Domain;
+
+namespace Hearts4Kids.Installers
+{
+    public class PersistenceInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<DbContext>()
+                                        .ImplementedBy<Hearts4KidsEntities>()
+                                        .LifestylePerWebRequest()
+                              );
+        }
+    }
+}
